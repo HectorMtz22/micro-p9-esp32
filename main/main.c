196d2a0ca690970f56a27f381ee418205c0b0906
Hector Martinez <hec.gre81@gmail.com>
2025-04-01 18:58:09 -0600

    Refactor (Buttons) into a component

--- main/main.c
+++ main/main.c
@@ -4,17 +4,9 @@
 
 #include "driver/gpio.h" 
 #include "display.h"
+#include "buttons.h"
 
 #define TIME_DELAY 300
-
-#define BTNS_INCREMENT GPIO_NUM_2
-#define BTNS_DECREMENT GPIO_NUM_15
-
-void btns_init(void);
-int btns_debounce(void);
-
-uint8_t prev_state_btn = 0;
-uint8_t actual_state_btn = 0;
 
 void app_main() {
   disp_init();
@@ -32,18 +24,4 @@ void app_main() {
     }
 
   }
-}
-
-void btns_init(void) {
-  gpio_set_direction(BTNS_INCREMENT, GPIO_MODE_INPUT);
-  gpio_set_direction(BTNS_DECREMENT, GPIO_MODE_INPUT);
-  return;
-}
-
-int btns_debounce(void) {
-  vTaskDelay(10 / portTICK_PERIOD_MS);
-  prev_state_btn = actual_state_btn;
-  actual_state_btn = gpio_get_level(BTNS_INCREMENT);
-
-  return !prev_state_btn && actual_state_btn;
 }
