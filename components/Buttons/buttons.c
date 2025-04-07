079c44c0c47141cceb84d06acad28ad7ef34d0a1
Hector Martinez <hec.gre81@gmail.com>
2025-04-07 03:37:33 -0600

    Add Showing led sequence

--- components/Buttons/buttons.c
+++ components/Buttons/buttons.c
@@ -2,15 +2,17 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 
+#include "leds.h"
+
 uint8_t prev_state_btn = 0;
 uint8_t actual_state_btn = 0;
 
-volatile int leds_state = 0;
+volatile int leds_state = 1;
 
 void btns_init(void) {
   // Configure buttons as interrupts
   gpio_config_t io_conf = {
-      .intr_type = GPIO_INTR_HIGH_LEVEL,
+      .intr_type = GPIO_INTR_POSEDGE, // Rising edge
       .mode = GPIO_MODE_INPUT,
       .pin_bit_mask = (1ULL << BTNS_INCREMENT) | (1ULL << BTNS_DECREMENT),
       .pull_down_en = GPIO_PULLDOWN_DISABLE,
@@ -26,11 +28,13 @@ void btns_init(void) {
 
 // ISR for button more
 void btns_isr_handler_more(void *arg) {
-  if (++leds_state > 5) leds_state = 5;
-
+  leds_state = leds_state << 1;
+  if (leds_state > 0b00100000) leds_state = 0b00100000;
+  leds_set(leds_state);
 }
 // ISR for button 1
 void btns_isr_handler_less(void *arg) {
-  if (--leds_state < 0) leds_state = 0;
-
+  leds_state = leds_state >> 1;
+  if (leds_state < 0b00000001) leds_state = 0b00000001;
+  leds_set(leds_state);
 }
