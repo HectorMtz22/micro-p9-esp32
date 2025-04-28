13e3cefbceeb8f3cfe7ac1c03cb7b4d8d9902126
Hector Mtz <hec.gre81@gmail.com>
2025-04-28 01:37:22 -0600

    Delete components that remain unused in this project

--- main/main.c
+++ main/main.c
@@ -3,8 +3,6 @@
 
 // Own Components
 #include "display.h"
-#include "buttons.h"
-#include "ledc.h"
 #include "leds.h"
 
 #define TIME_DELAY 500
@@ -13,26 +11,20 @@ void app_main() {
   // Desable interrupts
   esp_intr_disable(0);
   
-  // Initialize the display, buttons, and LED
+  // Initialize the display and LED
   leds_init();
   disp_init();
-  btns_init();
-  ledc_init();
 
   // Test before enabling interruptions
   xTaskCreate(leds_test, "LEDs Test", 2048, NULL, 3, NULL);
   disp_test();
 
-  // Register the ISR for the buttons
-  btns_isr_register();
-
   // Enable interrupts
   esp_intr_enable(0);
 
   while (true) {
-    for (int counter = 9; counter >= 0; counter--) {
+    for (int counter = 0; counter <= 9; counter++) {
       disp_show(counter);
-      ledc_update(counter);
       vTaskDelay(pdMS_TO_TICKS(TIME_DELAY));
     }
   }
