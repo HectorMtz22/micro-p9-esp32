8246101167eebda3198c10b7e88efde3d099e3ad
Hector Mtz <hec.gre81@gmail.com>
2025-04-28 01:40:57 -0600

    Configure led

--- components/Leds/include/leds.h
+++ components/Leds/include/leds.h
@@ -4,12 +4,7 @@
 #ifndef LEDS_H
 #define LEDS_H
 
-#define LEDS_1 GPIO_NUM_14
-#define LEDS_2 GPIO_NUM_27
-#define LEDS_3 GPIO_NUM_26
-#define LEDS_4 GPIO_NUM_25
-#define LEDS_5 GPIO_NUM_33
-#define LEDS_6 GPIO_NUM_32
+#define LEDS_1 GPIO_NUM_4
 
 #define TEST_DELAY 200
 
